2ca67a1b96bb7d093a35b4005fdef97b7bc46f2c
SaidFatah <said_designer@outlook.com>
2022-07-16 13:05:01 +0100

    added fade out animation support , and refactored execute Animation for time stamp

--- src/store/models/timeline/effects/executeAnimationForTimestamp.js
+++ src/store/models/timeline/effects/executeAnimationForTimestamp.js
@@ -1,73 +1,68 @@
 import { ANIMATIONS_TYPES } from "../../../../utils/constants";
 import { easeInOut, lerp } from "../../../../utils/utils";
 
-export default (dispatch,{ timestampValue }, state) => {
-    const shapes = state.timeline.shapes;
-    const timeStamps = [...state.timeline.timeStamps];
-    if (!timeStamps.length) return;
-    const targetTimeStamps = timeStamps.filter((timestamp) => {
-      return (
-        timestamp.time < timestampValue &&
-        timestampValue < timestamp.time + timestamp.duration
+export default (dispatch, { timestampValue }, state) => {
+  const shapes = state.timeline.shapes;
+  const timeStamps = [...state.timeline.timeStamps];
+  if (!timeStamps.length) return;
+  const targetTimeStamps = timeStamps.filter((timestamp) => {
+    return (
+      timestamp.time < timestampValue &&
+      timestampValue < timestamp.time + timestamp.duration
+    );
+  });
+
+  dispatch.timeline.UPDATE_CURRENT_TIME_STAMP({
+    currentTimeStamp: timestampValue,
+  });
+  targetTimeStamps.forEach((timeStamp) => {
+    // check if there is an animation linked to the time stamp
+    const targetAnimation =
+      state.timeline.timelineAnimations[timeStamp.animationId];
+    // execute animation on targeted shape
+    if (targetAnimation) {
+      const indexOfTargetShape = shapes.findIndex(
+        (shape) => shape.id === targetAnimation.shapeId
       );
-    });
 
-    dispatch.timeline.UPDATE_CURRENT_TIME_STAMP({
-      currentTimeStamp: timestampValue,
-    });
-    targetTimeStamps.forEach((timeStamp) => {
-      // check if there is an animation linked to the time stamp
-      const targetAnimation =
-        state.timeline.timelineAnimations[timeStamp.animationId];
-      // execute animation on targeted shape
-      if (targetAnimation) {
-        const indexOfTargetShape = shapes.findIndex(
-          (shape) => shape.id === targetAnimation.shapeId
+      if (indexOfTargetShape > -1) {
+        const type = targetAnimation.type;
+        const duration = targetAnimation.duration;
+        const elapsedTimeSinceAnimationStart = timestampValue - timeStamp.time;
+        const value = targetAnimation.value;
+        const prevValue = targetAnimation.prevValue;
+
+        const calculatedValue = lerp(
+          prevValue,
+          value,
+          easeInOut(elapsedTimeSinceAnimationStart / duration)
         );
+        const hasntFinishedYet =
+          prevValue > value
+            ? calculatedValue >= value
+            : calculatedValue <= value;
 
-        if (indexOfTargetShape > -1) {
-          const type = targetAnimation.type;
-          const duration = targetAnimation.duration;
-          const elapsedTimeSinceAnimationStart =
-            timestampValue - timeStamp.time;
-          if (
-            type === ANIMATIONS_TYPES.moveX ||
-            type === ANIMATIONS_TYPES.moveY
-          ) {
-            const value = targetAnimation.value;
-            const prevValue = targetAnimation.prevValue;
+        switch (type) {
+          case ANIMATIONS_TYPES.moveX:
+            if (hasntFinishedYet)
+              shapes[indexOfTargetShape].x = calculatedValue;
+            break;
+          case ANIMATIONS_TYPES.moveY:
+            if (hasntFinishedYet)
+              shapes[indexOfTargetShape].y = calculatedValue;
+            break;
+          case ANIMATIONS_TYPES.fadeIn:
+            shapes[indexOfTargetShape].opacity = calculatedValue;
+            break;
+          case ANIMATIONS_TYPES.fadeOut:
+            shapes[indexOfTargetShape].opacity = calculatedValue;
+            break;
 
-            
-            const calculatedValue = lerp(
-              prevValue,
-              value,
-              easeInOut(elapsedTimeSinceAnimationStart / duration)
-            );
-
-            const hasntFinishedYet =
-              prevValue > value
-                ? calculatedValue >= value
-                : calculatedValue <= value;
-
-            if (hasntFinishedYet) {
-              if (type === ANIMATIONS_TYPES.moveX)
-                shapes[indexOfTargetShape].x = calculatedValue;
-              if (type === ANIMATIONS_TYPES.moveY)
-                shapes[indexOfTargetShape].y = calculatedValue;
-            } else {
-              //if (isLastAnimation) cancelAnimationFrame(requestAnimationID);
-            }
-            dispatch.timeline.UPDATE_SHAPES({ shapes });
-          }else if (type === ANIMATIONS_TYPES.fadeIn){
-            const calculatedOpacity = lerp(
-              0,
-              1,
-              easeInOut(elapsedTimeSinceAnimationStart / duration)
-            );
-            shapes[indexOfTargetShape].opacity = calculatedOpacity;
-          }
-          //handleRefreshCallback(ctx);
+          default:
+            break;
         }
+        dispatch.timeline.UPDATE_SHAPES({ shapes });
       }
-    });
-  }
+    }
+  });
+};
